5deecb48397bc96fa7df7f37f76668d46ffa5c40
Ilie531 <trifanilie2002@gmail.com>
2021-09-02 12:43:05 +0200

    Create apunte.js

---
+++ apunte.js
@@ -0,0 +1,36 @@
+var Today = new Date();
+new Date(year,month,day,hours,minutes)
+//eg:Today.minutes
+const a = new Set (["x","y","z"])
+a.add("a")
+
+a.forEach(function(value({
+    alert(value)//makes an alert of all values
+})))
+
+console.log(value("a"));//"a" will show string, True will show boleal
+
+var x = ""//not the same as undefined or null
+undefined
+null
+
+Number("24")//return 24
+Number(False)//return 0
+Number(True)//return 1
+Number("1 1a")//return NaN
+String()
+
+
+Event
+
+onclick="func"
+onchange
+onmouseover
+onmouseout//the inverse of over, when the mouse leaves
+onkeydown//when you push keyboard
+onload
+
+window.onload = function(){
+
+    
+}
