5a14d9a3c6aa7cfc0f3a4414c5cfe974ceb01923
Ilie531 <trifanilie2002@gmail.com>
2021-09-02 10:05:26 +0200

    Did the exercise in readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Day-9
+Make webpage 1 button to display text, 1 to add style, 1 to add image.
---
+++ script.js
@@ -0,0 +1,9 @@
+var photo;
+photo="<img src=";
+photo +="'shrek.gif'>";
+
+var sheet = document.createElement('style');
+sheet.innerHTML = "body {background-image: url(rave.gif); background-size: cover;} p {font-size: 135%; color: rgb(88, 155, 0);} button {margin-bottom: 5mm; border-style: inset; border: lawngreen;}";
+window.onload = function() {
+    var context = new AudioContext();
+  }
