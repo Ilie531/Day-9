234ecc77a1662335bd1b77737fd022c109853148
Ilie531 <trifanilie2002@gmail.com>
2021-09-02 11:51:16 +0200

    Finished second exercise

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Day-9
 Make webpage 1 button to display text, 1 to add style, 1 to add image.
+
+name, second name, last name, where you live, contact info. Will add info to a list at a click of a button.
+what is a class?
+class Name{
+    constructor (x,y,z){
+        set the variables to use inside the class
+    }
+    this.x=x;
+    this.y=y
+}
+
+class Client {
+    constructor(name,lname,age,num){
+        this.name=name
+        this.lname=lname
+        this.age=age
+        this.num=num
+    }
+}
+
+let x= new.Client(an,be,20,...)
+push button on the left will show the people we added
--- script.js
+++ script.js
@@ -4,6 +4,58 @@ photo +="'shrek.gif'>";
 
 var sheet = document.createElement('style');
 sheet.innerHTML = "body {background-image: url(rave.gif); background-size: cover;} p {font-size: 135%; color: rgb(88, 155, 0);} button {margin-bottom: 5mm; border-style: inset; border: lawngreen;}";
-window.onload = function() {
-    var context = new AudioContext();
-  }
+
+
+
+class Client {
+    constructor(name,age,adr,mp){
+        this.name = name;
+        this.age = age;
+        this.adr = adr;
+        this.mp = mp;
+    };
+    showed(){
+        return String(this.name)+" "+ String(this.age)+" "+String(this.adr)+" "+String(this.mp); 
+    }
+};
+
+
+
+//var cliente = new.Client("Pedro",15,"Granada","999999999");
+//function any()
+
+//function Add(array,cliente){
+  //  array;
+//};
+
+function TakeData(cs){
+    let namei = document.getElementById('nameEntry').value;
+    let agei = document.getElementById('ageEntry').value;
+    let adri = document.getElementById('adrEntry').value;
+    let mpi = document.getElementById('mpEntry').value;
+    
+    var cliente = new Client(namei,agei,adri,mpi);
+    cs.push(cliente);
+    
+}
+function Show(cs){
+    var text="";
+    for (i of cs) {
+       text+=i.showed()+"<br>";
+    }
+    document.getElementById('text').innerHTML=text;
+}
+let cs = [];
+class Operacion {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+    }
+
+    aa(){
+        return Number(this.x + this.y)
+    }
+}
+
+var sum =new Operacion(5,6)
+console.log(sum.aa()) 
